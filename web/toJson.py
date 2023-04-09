217793899e90474871e44b322dee08e995d7ce1e
Felix L <97065023+Felix-Lar@users.noreply.github.com>
2023-04-09 11:44:28 -0400

    Add files via upload
    3D force directed graph prelim,
    data from correlated matrix,
    script to convert csv to json

---
+++ web/toJson.py
@@ -0,0 +1,66 @@
+## Convert csv to JSON for 3D force-direcetd graph
+
+
+
+import pandas as pd
+
+
+data = pd.read_csv('corr_graph.csv')
+data.copy()
+data_filt = data[data['Weight']>0]
+
+nodes = []
+group_number = []
+
+count = 1
+
+for i in range(len(data_filt)):
+    
+    if data_filt['Source'].iloc[i] not in nodes:
+        nodes.append(data_filt['Source'].iloc[i])
+        group_number.append(count)
+        count+=1
+    if data_filt['Target'].iloc[i] not in nodes:
+        nodes.append(data_filt['Target'].iloc[i])
+        group_number.append(count)
+        count+=1
+
+
+
+
+node_data = pd.DataFrame()
+node_data['id'] = nodes
+node_data['group'] = group_number
+
+edge_source = []
+edge_target = []
+edge_value = []
+
+for i in range(len(data_filt)):
+    edge_source.append(data_filt['Source'].iloc[i])
+    edge_target.append(data_filt['Target'].iloc[i])
+    edge_value.append(data_filt['Weight'].iloc[i])
+    
+    
+
+
+edge_data = pd.DataFrame()
+edge_data['source'] = edge_source
+edge_data['target'] = edge_target
+edge_data['value'] = edge_value
+
+
+edge_data.to_json('/Users/felixlarouche/Documents/Georgia_Tech/CSE6242/Group_Project/dva_project-main/web3..', orient='records')
+
+
+# print(edge_data)
+
+
+
+
+
+# data_comb = data_filt['Source']
+
+
+# print(len(data_filt))
+
